1e4fe6c458bd9b893440316c9475b0646e18ee1e
NeofastFTL <trovagetron@gmail.com>
2022-08-21 20:13:26 +0200

    Added silicon

--- src/main/java/com/neofast/tech_revised/item/ModItems.java
+++ src/main/java/com/neofast/tech_revised/item/ModItems.java
@@ -3,14 +3,10 @@ package com.neofast.tech_revised.item;
 import com.neofast.tech_revised.TechRevised;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.apache.logging.log4j.core.async.DefaultAsyncQueueFullPolicy;
-import static net.minecraftforge.registries.DeferredRegister.*;
-import static net.minecraftforge.registries.ForgeRegistries.*;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -18,7 +14,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> CHIPSET_1 = ITEMS.register("chipset_1",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
-    public static final RegistryObject<Item>  = ITEMS.register("chipset_1",
+    public static final RegistryObject<Item> SILICON = ITEMS.register("silicon",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 

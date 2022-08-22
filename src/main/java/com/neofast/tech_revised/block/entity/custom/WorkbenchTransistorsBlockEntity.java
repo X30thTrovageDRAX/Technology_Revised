d0e5f115267a0c75e828cd671c8c5e985576a820
NeofastFTL <trovagetron@gmail.com>
2022-08-22 14:41:36 +0200

    Added stuff extra

--- src/main/java/com/neofast/tech_revised/block/entity/custom/WorkbenchTransistorsBlockEntity.java
+++ src/main/java/com/neofast/tech_revised/block/entity/custom/WorkbenchTransistorsBlockEntity.java
@@ -16,9 +16,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.PotionUtils;
-import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -35,7 +32,7 @@ import java.util.Random;
 
 
 public class WorkbenchTransistorsBlockEntity extends BlockEntity implements MenuProvider {
-    private final ItemStackHandler itemHandler = new ItemStackHandler(6) {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(5) {
         @Override
         protected void onContentsChanged(int slot) {
             setChanged();
@@ -119,7 +116,7 @@ public class WorkbenchTransistorsBlockEntity extends BlockEntity implements Menu
         entity.itemHandler.getStackInSlot(5).hurt(1, new Random(), null);
 
         entity.itemHandler.setStackInSlot(5, new ItemStack(ModBlocks.COMPRESSED_SILICON.get(),
-                 entity.itemHandler.getStackInSlot(6).getCount() + 1));
+                 entity.itemHandler.getStackInSlot(5).getCount() + 1));
     }
 
     private static boolean hasRecipe(WorkbenchTransistorsBlockEntity entity) {
@@ -134,6 +131,6 @@ public class WorkbenchTransistorsBlockEntity extends BlockEntity implements Menu
     }
 
     private static boolean hasNotReachedStackLimit(WorkbenchTransistorsBlockEntity entity) {
-        return entity.itemHandler.getStackInSlot(6).getCount() < entity.itemHandler.getStackInSlot(6).getMaxStackSize();
+        return entity.itemHandler.getStackInSlot(5).getCount() < entity.itemHandler.getStackInSlot(5).getMaxStackSize();
     }
 }
